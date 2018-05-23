ad6d0da44529ddcf812f8cc7a649dbb3c22c1c33
zl <zl@zldeMacBook-Pro.local>
2018-05-23 18:05:05 +0800

    add model

--- model.py
+++ model.py
@@ -23,11 +23,32 @@ class EncoderCNN(nn.Module):
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
+        super(DecoderRNN, self).__init__()
+        self.hidden_dim = hidden_size
+        self.embeddings = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size)
+        
+        self.hidden2sentence = nn.Linear(hidden_size, num_layers)
+        self.hidden = self.init_hidden()
+        
+    def init_hidden(self):
+        ''' At the start of training, we need to initialize a hidden state;
+           there will be none because the hidden state is formed based on perviously seen data.
+           So, this function defines a hidden state with all zeroes and of a specified size.'''
+        # The axes dimensions are (n_layers, batch_size, hidden_dim)
+        return (Variable(torch.zeros(1, 1, self.hidden_dim)),
+                Variable(torch.zeros(1, 1, self.hidden_dim)))
     
     def forward(self, features, captions):
-        pass
+        embeds = self.embeddings(captions)
+        lstm_out, self.hidden = self.lstm(
+            features.view(len(sentence), 1, -1), self.hidden)
+        
+        tag_outputs = self.hidden2sentence(lstm_out.view(len(features), -1))
+        tag_scores = F.log_softmax(tag_outputs, dim=len(captions))
+        
+        return tag_scores
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        pass
+        return self.forward(inputs, max_len)
