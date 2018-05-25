6ea244eefdf29a36dca9da9b5ddb8e2ab588eb91
Ubuntu <ubuntu@ip-172-31-3-238.ec2.internal>
2018-05-25 07:04:15 +0000

    big change

--- data_loader.py
+++ data_loader.py
@@ -1,153 +1,152 @@
-import nltk
-import os
 import torch
+import torchvision.transforms as transforms
 import torch.utils.data as data
-from vocabulary import Vocabulary
-from PIL import Image
-from pycocotools.coco import COCO
+import os
+import pickle
 import numpy as np
-from tqdm import tqdm
-import random
-import json
+import nltk
+from PIL import Image
 
-def get_loader(transform,
-               mode='train',
-               batch_size=1,
-               vocab_threshold=None,
-               vocab_file='./vocab.pkl',
-               start_word="<start>",
-               end_word="<end>",
-               unk_word="<unk>",
-               vocab_from_file=True,
-               num_workers=0,
-               cocoapi_loc=''):
-    """Returns the data loader.
-    Args:
-      transform: Image transform.
-      mode: One of 'train' or 'test'.
-      batch_size: Batch size (if in testing mode, must have batch_size=1).
-      vocab_threshold: Minimum word count threshold.
-      vocab_file: File containing the vocabulary. 
-      start_word: Special word denoting sentence start.
-      end_word: Special word denoting sentence end.
-      unk_word: Special word denoting unknown words.
-      vocab_from_file: If False, create vocab from scratch & override any existing vocab_file.
-                       If True, load vocab from from existing vocab_file, if it exists.
-      num_workers: Number of subprocesses to use for data loading 
-      cocoapi_loc: The location of the folder containing the COCO API: https://github.com/cocodataset/cocoapi
-    """
-    
-    assert mode in ['train', 'test'], "mode must be one of 'train' or 'test'."
-    if vocab_from_file==False: assert mode=='train', "To generate vocab from captions file, must be in training mode (mode='train')."
+from pycocotools.coco import COCO
 
-    # Based on mode (train, val, test), obtain img_folder and annotations_file.
-    if mode == 'train':
-        if vocab_from_file==True: assert os.path.exists(vocab_file), "vocab_file does not exist.  Change vocab_from_file to False to create vocab_file."
-        img_folder = os.path.join(cocoapi_loc, 'images/train2014/')
-        annotations_file = os.path.join(cocoapi_loc, 'annotations/captions_train2014.json')
-    if mode == 'test':
-        assert batch_size==1, "Please change batch_size to 1 if testing your model."
-        assert os.path.exists(vocab_file), "Must first generate vocab.pkl from training data."
-        assert vocab_from_file==True, "Change vocab_from_file to True."
-        img_folder = os.path.join(cocoapi_loc, 'images/test2014/')
-        annotations_file = os.path.join(cocoapi_loc, 'annotations/image_info_test2014.json')
+class Vocabulary(object):
+    """Simple vocabulary wrapper."""
+    def __init__(self):
+        self.word2idx = {}
+        self.idx2word = {}
+        self.idx = 0
 
-    # COCO caption dataset.
-    print('xx{}'.format(img_folder))
-    dataset = CoCoDataset(transform=transform,
-                          mode=mode,
-                          batch_size=batch_size,
-                          vocab_threshold=vocab_threshold,
-                          vocab_file=vocab_file,
-                          start_word=start_word,
-                          end_word=end_word,
-                          unk_word=unk_word,
-                          annotations_file=annotations_file,
-                          vocab_from_file=vocab_from_file,
-                          img_folder=img_folder)
+    def add_word(self, word):
+        if not word in self.word2idx:
+            self.word2idx[word] = self.idx
+            self.idx2word[self.idx] = word
+            self.idx += 1
 
-    if mode == 'train':
-        # Randomly sample a caption length, and sample indices with that length.
-        indices = dataset.get_train_indices()
-        # Create and assign a batch sampler to retrieve a batch with the sampled indices.
-        initial_sampler = data.sampler.SubsetRandomSampler(indices=indices)
-        # data loader for COCO dataset.
-        data_loader = data.DataLoader(dataset=dataset, 
-                                      num_workers=num_workers,
-                                      batch_sampler=data.sampler.BatchSampler(sampler=initial_sampler,
-                                                                              batch_size=dataset.batch_size,
-                                                                              drop_last=False))
-    else:
-        data_loader = data.DataLoader(dataset=dataset,
-                                      batch_size=dataset.batch_size,
-                                      shuffle=True,
-                                      num_workers=num_workers)
-
-    return data_loader
-
-class CoCoDataset(data.Dataset):
-    
-    def __init__(self, transform, mode, batch_size, vocab_threshold, vocab_file, start_word, 
-        end_word, unk_word, annotations_file, vocab_from_file, img_folder):
-        self.transform = transform
-        self.mode = mode
-        self.batch_size = batch_size
-        self.vocab = Vocabulary(vocab_threshold, vocab_file, start_word,
-            end_word, unk_word, annotations_file, vocab_from_file)
-        self.img_folder = img_folder
-        if self.mode == 'train':
-            self.coco = COCO(annotations_file)
-            self.ids = list(self.coco.anns.keys())
-            print('Obtaining caption lengths...')
-            all_tokens = [nltk.tokenize.word_tokenize(str(self.coco.anns[self.ids[index]]['caption']).lower()) for index in tqdm(np.arange(len(self.ids)))]
-            self.caption_lengths = [len(token) for token in all_tokens]
-        else:
-            test_info = json.loads(open(annotations_file).read())
-            self.paths = [item['file_name'] for item in test_info['images']]
-        
-    def __getitem__(self, index):
-        # obtain image and caption if in training mode
-        if self.mode == 'train':
-            ann_id = self.ids[index]
-            caption = self.coco.anns[ann_id]['caption']
-            img_id = self.coco.anns[ann_id]['image_id']
-            path = self.coco.loadImgs(img_id)[0]['file_name']
-
-            # Convert image to tensor and pre-process using transform
-            image = Image.open(os.path.join(self.img_folder, path)).convert('RGB')
-            image = self.transform(image)
-
-            # Convert caption to tensor of word ids.
-            tokens = nltk.tokenize.word_tokenize(str(caption).lower())
-            caption = []
-            caption.append(self.vocab(self.vocab.start_word))
-            caption.extend([self.vocab(token) for token in tokens])
-            caption.append(self.vocab(self.vocab.end_word))
-            caption = torch.Tensor(caption).long()
-
-            # return pre-processed image and caption tensors
-            return image, caption
-
-        # obtain image if in test mode
-        else:
-            path = self.paths[index]
-
-            # Convert image to tensor and pre-process using transform
-            PIL_image = Image.open(os.path.join(self.img_folder, path)).convert('RGB')
-            orig_image = np.array(PIL_image)
-            image = self.transform(PIL_image)
-
-            # return original image and pre-processed image tensor
-            return orig_image, image
-
-    def get_train_indices(self):
-        sel_length = np.random.choice(self.caption_lengths)
-        all_indices = np.where([self.caption_lengths[i] == sel_length for i in np.arange(len(self.caption_lengths))])[0]
-        indices = list(np.random.choice(all_indices, size=self.batch_size))
-        return indices
+    def __call__(self, word):
+        if not word in self.word2idx:
+            return self.word2idx['<unk>']
+        return self.word2idx[word]
 
     def __len__(self):
-        if self.mode == 'train':
-            return len(self.ids)
-        else:
-            return len(self.paths)
+        return len(self.word2idx)
+
+def build_vocab(json, threshold):
+    """Build a simple vocabulary wrapper."""
+    coco = COCO(json)
+    counter = Counter()
+    ids = coco.anns.keys()
+    for i, id in enumerate(ids):
+        caption = str(coco.anns[id]['caption'])
+        tokens = nltk.tokenize.word_tokenize(caption.lower())
+        counter.update(tokens)
+
+        if (i+1) % 1000 == 0:
+            print("[{}/{}] Tokenized the captions.".format(i+1, len(ids)))
+
+    # If the word frequency is less than 'threshold', then the word is discarded.
+    words = [word for word, cnt in counter.items() if cnt >= threshold]
+
+    # Create a vocab wrapper and add some special tokens.
+    vocab = Vocabulary()
+    vocab.add_word('<pad>')
+    vocab.add_word('<start>')
+    vocab.add_word('<end>')
+    vocab.add_word('<unk>')
+
+    # Add the words to the vocabulary.
+    for i, word in enumerate(words):
+        vocab.add_word(word)
+    return vocab
+class CocoDataset(data.Dataset):
+    """COCO Custom Dataset compatible with torch.utils.data.DataLoader."""
+    def __init__(self, root, json, vocab, transform=None):
+        """Set the path for images, captions and vocabulary wrapper.
+        
+        Args:
+            root: image directory.
+            json: coco annotation file path.
+            vocab: vocabulary wrapper.
+            transform: image transformer.
+        """
+        self.root = root
+        self.coco = COCO(json)
+        self.ids = list(self.coco.anns.keys())
+        self.vocab = vocab
+        self.transform = transform
+
+    def __getitem__(self, index):
+        """Returns one data pair (image and caption)."""
+        coco = self.coco
+        vocab = self.vocab
+        ann_id = self.ids[index]
+        caption = coco.anns[ann_id]['caption']
+        img_id = coco.anns[ann_id]['image_id']
+        path = coco.loadImgs(img_id)[0]['file_name']
+
+        image = Image.open(os.path.join(self.root, path)).convert('RGB')
+        if self.transform is not None:
+            image = self.transform(image)
+
+        # Convert caption (string) to word ids.
+        tokens = nltk.tokenize.word_tokenize(str(caption).lower())
+        caption = []
+        caption.append(vocab('<start>'))
+        caption.extend([vocab(token) for token in tokens])
+        caption.append(vocab('<end>'))
+        target = torch.Tensor(caption)
+        return image, target
+
+    def __len__(self):
+        return len(self.ids)
+
+
+def collate_fn(data):
+    """Creates mini-batch tensors from the list of tuples (image, caption).
+    
+    We should build custom collate_fn rather than using default collate_fn, 
+    because merging caption (including padding) is not supported in default.
+
+    Args:
+        data: list of tuple (image, caption). 
+            - image: torch tensor of shape (3, 256, 256).
+            - caption: torch tensor of shape (?); variable length.
+
+    Returns:
+        images: torch tensor of shape (batch_size, 3, 256, 256).
+        targets: torch tensor of shape (batch_size, padded_length).
+        lengths: list; valid length for each padded caption.
+    """
+    # Sort a data list by caption length (descending order).
+    data.sort(key=lambda x: len(x[1]), reverse=True)
+    images, captions = zip(*data)
+
+    # Merge images (from tuple of 3D tensor to 4D tensor).
+    images = torch.stack(images, 0)
+
+    # Merge captions (from tuple of 1D tensor to 2D tensor).
+    lengths = [len(cap) for cap in captions]
+    targets = torch.zeros(len(captions), max(lengths)).long()
+    for i, cap in enumerate(captions):
+        end = lengths[i]
+        targets[i, :end] = cap[:end]        
+    return images, targets, lengths
+
+def get_loader(root, json, vocab, transform, batch_size, shuffle, num_workers):
+    """Returns torch.utils.data.DataLoader for custom coco dataset."""
+    # COCO caption dataset
+    coco = CocoDataset(root=root,
+                       json=json,
+                       vocab=vocab,
+                       transform=transform)
+    
+    # Data loader for COCO dataset
+    # This will return (images, captions, lengths) for each iteration.
+    # images: a tensor of shape (batch_size, 3, 224, 224).
+    # captions: a tensor of shape (batch_size, padded_length).
+    # lengths: a list indicating valid length for each caption. length is (batch_size).
+    data_loader = torch.utils.data.DataLoader(dataset=coco, 
+                                              batch_size=batch_size,
+                                              shuffle=shuffle,
+                                              num_workers=num_workers,
+                                              collate_fn=collate_fn)
+    return data_loader
